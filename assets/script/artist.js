9359188d16894698d0e326fdf2029d7bcdde2a92
AndreaCeliberti <andrea.celiberti2@gmail.com>
2025-09-24 14:46:44 +0200

    scrollbar

--- assets/script/artist.js
+++ assets/script/artist.js
@@ -1,6 +1,8 @@
-window.addEventListener("scroll", function () {
-  const heroImage = document.getElementById("heroImage");
-  const scrollY = window.scrollY;
+const centerFeed = document.getElementById("centralIndex");
+const heroImage = document.getElementById("heroImage");
+
+centerFeed.addEventListener("scroll", function () {
+  const scrollY = centerFeed.scrollTop;
   const maxHeight = 100;
   let opacity = 1 - scrollY / maxHeight;
 
